9ba8f85f2db1dc160c61aa8369429b21f69876af
Matteo Ciapparelli <ciapparellimatteo@gmail.com>
2024-01-24 00:44:15 +0100

    Added tests

---
+++ tests/AspNetCore.SwaggerUI.Themes.Tests/Program.cs
@@ -0,0 +1,28 @@
+﻿using AspNetCore.SwaggerUI.Themes;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using static AspNetCore.SwaggerUI.Themes.StyleProvider;
+
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+{
+    EnvironmentName = Environments.Staging
+});
+var app = builder.Build();
+
+// Add style endpoint
+RegisterTestStyleEndpoint();
+
+await app.RunAsync();
+
+void RegisterTestStyleEndpoint()
+{
+    var style = Style.Dark;
+    var fullPath = StylePath + style.FileName;
+    AddGetEndpoint(app, fullPath, GetResourceText(style.FileName), style.FormatText);
+}
+
+public partial class Program
+{
+    protected Program()
+    { }
+}
---
+++ tests/AspNetCore.SwaggerUI.Themes.Tests/StyleProviderWebApplicationFactory.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+
+namespace AspNetCore.SwaggerUI.Themes.Tests;
+
+/// <summary>
+/// Factory for bootstrapping an application in memory for functional end to end tests.
+/// </summary>
+/// <typeparam name="TProgram">
+/// A type in the entry point assembly of the application. Typically the Program classes can be used.
+/// </typeparam>
+public abstract class StyleProviderWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram>
+    where TProgram : class
+{
+    /// <summary>
+    /// HTTP client for executing tests.
+    /// </summary>
+    protected HttpClient Client { get; set; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="StyleProviderWebApplicationFactory{TProgram}"/> class.
+    /// </summary>
+    protected StyleProviderWebApplicationFactory()
+    {
+        Client = CreateClient(new WebApplicationFactoryClientOptions
+        {
+            AllowAutoRedirect = false
+        });
+    }
+
+    /// <inheritdoc/>
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        return base.CreateHost(builder);
+    }
+}
