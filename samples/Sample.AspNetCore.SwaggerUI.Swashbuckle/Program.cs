94d0a28bdb59c3ddadb9bca37bb6976e308c73a2
Matteo Ciapparelli <ciapparellimatteo@gmail.com>
2024-02-10 00:27:12 +0100

    Add more examples

--- samples/Sample.AspNetCore.SwaggerUI.Swashbuckle/Program.cs
+++ samples/Sample.AspNetCore.SwaggerUI.Swashbuckle/Program.cs
@@ -1,17 +1,68 @@
 using AspNetCore.SwaggerUI.Themes;
+using Microsoft.OpenApi.Models;
+using System.ComponentModel.DataAnnotations;
+
 #if NET6_0 || NET7_0
+
 using Swashbuckle.AspNetCore.Annotations;
+
 #endif
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
 
 // Add services to the container. Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-#if NET7_0_OR_GREATER
-builder.Services.AddSwaggerGen();
-#else
-builder.Services.AddSwaggerGen(opts => opts.EnableAnnotations());
-#endif
+builder.Services.AddSwaggerGen(options =>
+    {
+        #if NET6_0 || NET7_0
+        options.EnableAnnotations();
+        #endif
+        options.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Version = "v1",
+            Title = "ToDo API",
+            Description = "An ASP.NET Core Web API for testing",
+            TermsOfService = new Uri("https://example.com/terms"),
+            Contact = new OpenApiContact
+            {
+                Name = "Example Contact",
+                Url = new Uri("https://example.com/contact")
+            },
+            License = new OpenApiLicense
+            {
+                Name = "Example License",
+                Url = new Uri("https://example.com/license")
+            }
+        });
+        options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Description = @"JWT Authorization header using the Bearer scheme.
+                          Enter 'Bearer' [space] and then your token in the text input below.
+                          Example: 'Bearer 12345abcdef'",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer"
+        });
+        options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    },
+                    Scheme = "oauth2",
+                    Name = "Bearer",
+                    In = ParameterLocation.Header,
+                },
+                new List<string>()
+            }
+        });
+    });
 
 var app = builder.Build();
 
@@ -19,7 +70,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(Style.Dark, c => c.DocumentTitle = "Prova");
+    app.UseSwaggerUI(Style.Dark, c => c.DocumentTitle = "Sample Title");
 }
 
 app.UseHttpsRedirection();
@@ -50,9 +101,45 @@ app.MapGet("/weatherforecast", () =>
 .WithMetadata(new SwaggerOperationAttribute(summary: "Summary", description: "Description Test"));
 #endif
 
+app.MapPost("/weatherforecast/create", (WeatherForecast model) => model)
+.WithName("PostWeatherForecast")
+#if NET7_0_OR_GREATER
+.WithSummary("Summary")
+.WithDescription("Description Test")
+.WithOpenApi();
+#else
+.WithMetadata(new SwaggerOperationAttribute(summary: "Summary", description: "Description Test"));
+#endif
+
+app.MapPut("/weatherforecast/update", (int id, WeatherForecast model) => (id, model))
+.WithName("PutWeatherForecast")
+#if NET7_0_OR_GREATER
+.WithSummary("Summary")
+.WithDescription("Description Test")
+.WithOpenApi();
+#else
+.WithMetadata(new SwaggerOperationAttribute(summary: "Summary", description: "Description Test"));
+#endif
+
+app.MapDelete("/weatherforecast/delete", (int id) => id)
+.WithName("DeleteWeatherForecast")
+#if NET7_0_OR_GREATER
+.WithSummary("Summary")
+.WithDescription("Description Test")
+.WithOpenApi();
+#else
+.WithMetadata(new SwaggerOperationAttribute(summary: "Summary", description: "Description Test"));
+#endif
+
+app.MapControllers();
+
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string Summary)
+public record WeatherForecast(DateOnly Date, int TemperatureC, string Summary)
 {
+    [Obsolete("test")]
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
+
+[Obsolete("test")]
+public record Sample(WeatherForecast WeatherForecast, [Required] double Value);
