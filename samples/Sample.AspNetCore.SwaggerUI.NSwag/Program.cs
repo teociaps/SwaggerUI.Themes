61380eb9e90118f75c35c074acf699eba2e684f3
Matteo Ciapparelli <ciapparellimatteo@gmail.com>
2024-08-16 01:09:18 +0200

    Reorganized nswag generator in sample proj

--- samples/Sample.AspNetCore.SwaggerUI.NSwag/Program.cs
+++ samples/Sample.AspNetCore.SwaggerUI.NSwag/Program.cs
@@ -1,56 +1,10 @@
-using NSwag;
-using NSwag.Generation.Processors.Security;
 using Sample.AspNetCore.SwaggerUI.NSwag;
 using SwaggerThemes;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
-builder.Services.AddOpenApiDocument(options =>
-{
-    options.PostProcess = document =>
-    {
-        document.Info = new OpenApiInfo
-        {
-            Version = "v1",
-            Title = "ToDo API",
-            Description = "An ASP.NET Core Web API for managing ToDo items",
-            TermsOfService = "https://example.com/terms",
-            Contact = new OpenApiContact
-            {
-                Name = "Example Contact",
-                Url = "https://example.com/contact"
-            },
-            License = new OpenApiLicense
-            {
-                Name = "Example License",
-                Url = "https://example.com/license"
-            }
-        };
-    };
-    options.AddSecurity("bearer", [], new OpenApiSecurityScheme
-    {
-        Type = OpenApiSecuritySchemeType.OAuth2,
-        Description = "My Authentication",
-        Flow = OpenApiOAuth2Flow.Implicit,
-        Flows = new OpenApiOAuthFlows()
-        {
-            Implicit = new OpenApiOAuthFlow()
-            {
-                Scopes = new Dictionary<string, string>
-                {
-                    { "read", "Read access to protected resources" },
-                    { "write", "Write access to protected resources" }
-                },
-                AuthorizationUrl = "https://localhost:44333/core/connect/authorize",
-                TokenUrl = "https://localhost:44333/core/connect/token"
-            },
-        }
-    });
-
-    options.OperationProcessors.Add(
-        new AspNetCoreOperationSecurityScopeProcessor("bearer"));
-});
+builder.Services.AddOpenApiDocument(OpenApiDocGenConfigurer.Configure);
 
 var app = builder.Build();
 
